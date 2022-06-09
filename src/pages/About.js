db2ca36019eda2a272962cfa6a3394790f932df4
Sruthi <69165762+sruthipapanasa@users.noreply.github.com>
2022-06-08 17:20:51 -0700

    started styling about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import "./About.css";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Sruthi Papanasa</div>
+        <div className="brief_description">
+          <ul>
+            <li>Cats</li>
+            <li>Swimming</li>
+            <li>Painting</li>
+        </ul>
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     )
   }
